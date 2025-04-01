c7bd771b73a88c6385b78a90288df3f10eda3648
Liam053 <Liam@kleinhalle.nl>
2025-04-01 09:13:12 +0200

    Begin aan opdracht 1 van asp security

---
+++ Semester6LiamKleinhalle/Semester6LiamKleinhalle/Controllers/S6ASPSEC01Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Semester6LiamKleinhalle.Helpers;
+
+namespace Semester6LiamKleinhalle.Controllers
+{
+    public class S6ASPSEC01Controller : Controller
+    {
+        // Actie voor het weergeven van de encryptiepagina
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Actie voor het versleutelen van de tekst
+        [HttpPost]
+        public IActionResult Encrypt(string input, int key)
+        {
+            // Versleutel de tekst
+            string encryptedText = EncryptionFunctions.Encrypt(input, key);
+            // Stuur de versleutelde tekst naar de view via ViewBag
+            ViewBag.EncryptedText = encryptedText;
+            // Laad de indexpagina met de versleutelde tekst
+            return View("Index");
+        }
+
+        // Actie voor het decrypten van de tekst (optioneel voor de decryptie functionaliteit)
+        [HttpPost]
+        public IActionResult Decrypt(string input, int key)
+        {
+            // Decrypt de tekst
+            string decryptedText = EncryptionFunctions.Decrypt(input, key);
+            // Stuur de gedecryptte tekst naar de view via ViewBag
+            ViewBag.DecryptedText = decryptedText;
+            // Laad de indexpagina met de gedecryptte tekst
+            return View("Index");
+        }
+    }
+}
---
+++ Semester6LiamKleinhalle/Semester6LiamKleinhalle/Helpers/EncryptionFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Semester6LiamKleinhalle.Helpers
+{
+    public static class EncryptionFunctions
+    {
+        // Charset voor encryptie en decryptie
+        private const string Charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        // Encryptie functie
+        public static string Encrypt(string input, int key)
+        {
+            return new string(input.Select(c =>
+            {
+                int index = Charset.IndexOf(c);
+                return index >= 0 ? Charset[(index + key) % Charset.Length] : c;
+            }).ToArray());
+        }
+
+        // Decryptie functie
+        public static string Decrypt(string input, int key)
+        {
+            return new string(input.Select(c =>
+            {
+                int index = Charset.IndexOf(c);
+                return index >= 0 ? Charset[(index - key + Charset.Length) % Charset.Length] : c;
+            }).ToArray());
+        }
+    }
+}
